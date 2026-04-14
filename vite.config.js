add960e5129bd1267b6e1a63312993db6b86f6d9
avina <avinashmahto591@gmail.com>
2026-04-14 21:27:14 +0530

    Updated About section

--- vite.config.js
+++ vite.config.js
@@ -17,10 +17,20 @@
 //     tailwindcss(),
 //   ],
 // })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
